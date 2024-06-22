22675c792e5e9a017778be6c47dace608989fcee
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-22 18:03:23 +0530

    Adding module js instead of commmon js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
-const express = require('express');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,6 +11,6 @@ app.get('/', (req, res) => {
 
 app.get('/twitter', (req, res) => {});
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
