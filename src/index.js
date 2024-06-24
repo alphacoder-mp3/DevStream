144cc7951f729d779e5f52b2d058ba03306bd0ac
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-24 21:07:05 +0530

    middlewares and best production code practices

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import express from 'express';
 import { config } from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 config({ path: './env' });
-connectDB();
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Function to start the server
+const startServer = async () => {
+  try {
+    // Connect to the database
+    await connectDB();
 
-app.get('/twitter', (req, res) => {});
+    app.on('error', error => {
+      console.log('ERR:', error);
+      throw error;
+    });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    // Set the port from environment variables or default to 8000
+    const port = process.env.PORT || 8000;
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1); // Exit the process with an error code
+  }
+};
+
+// Start the server
+startServer();
