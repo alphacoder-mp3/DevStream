e137e5fdc370e5aeb7c8979da65cc7791511a1dc
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-28 21:22:38 +0530

    Added basic user register route

--- src/app.js
+++ src/app.js
@@ -19,4 +19,9 @@ app.use(express.static('public'));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from './routes/user.routes.js';
+
+app.use('/api/v1/users', userRouter);
+
 export { app };
