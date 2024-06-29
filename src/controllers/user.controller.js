bc10a3f6b4641546f52c3a1d850dfc116929906c
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-29 16:06:02 +0530

    Resolve login bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -94,7 +94,7 @@ const loginUser = asyncHandler(async (req, res) => {
   // send cookie
   const { username, email, password } = req.body;
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, 'username or email is required');
   }
 
