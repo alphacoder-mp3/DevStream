f21095cdb0ad5af677d83e1bc280a88c11142b11
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-02 05:11:14 +0530

    Add missing imports

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,8 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
