f1bb76bc7526e38818f440601c57688e778ce2c2
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-02 09:50:32 +0530

    Bug fix in logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,7 +146,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     req.user._id,
     {
       $set: {
-        refreshToken: undefined,
+        refreshToken: 1, // removes field from the document
       },
     },
     {
